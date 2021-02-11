b10710c5d13c49126c70ccee77b2ade87624b48e
tt4623 <sltebr33zy@gmail.com>
2021-02-10 18:55:36 -0600

    cleaning

--- server.js
+++ server.js
@@ -7,20 +7,19 @@ const PORT = process.env.PORT || 8080;
 
 // setup express server var
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // setup connection to mongoose db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-plannerDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"))
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
